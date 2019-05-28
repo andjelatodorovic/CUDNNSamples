ec4604ad901adc8427b8c016bb8a9517afc37d13
Anđela Todorović <andjelatodorovicc@gmail.com>
2019-05-28 17:18:26 +0200

    Add files via upload

---
+++ Reduction/cudnn.h
@@ -0,0 +1,16 @@
+#ifndef CUDNN_H_
+#define CUDNN_H_
+//AUTHOR: @kobrica
+
+#define CUDA_CHECK_RETURN(value) {											\
+	cudaError_t _m_cudaStat = value;										\
+	if (_m_cudaStat != cudaSuccess) {										\
+		fprintf(stderr, "Error %s at line %d in file %s\n",					\
+				cudaGetErrorString(_m_cudaStat), __LINE__, __FILE__);		\
+		exit(1);															\
+	} }
+
+
+
+
+#endif
